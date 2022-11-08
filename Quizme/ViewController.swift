2f8bc0242cfae0c0020d75cc3ef3a09b0e760cc2
Mateusz Urbas <mateusz.urbas@siepomaga.pl>
2022-11-08 18:05:28 +0100

    init proj

---
+++ Quizme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizme
+//
+//  Created by Mateusz Urbas on 08/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
